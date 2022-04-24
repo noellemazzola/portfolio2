d49f84db25c02de98906bb9cc478f8768b58339a
noellemazzola <97685560+noellemazzola@users.noreply.github.com>
2022-04-23 21:57:58 -0400

    Create form.js

---
+++ todo/src/components/form.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+function Form({ setInputText, toDos, settoDos, inputText }) {
+    const inputTextHandler = (e) => {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+
+    const submitToDoHandler = (e) => {
+        e.preventDefault();
+        settoDos([
+            ...toDos, {text: inputText, completed: false, id: Math.random() *  1000}
+        ]);
+        setInputText("");
+    };
+
+    return (
+        <form>
+            <input onChange={inputTextHandler} type= "text" className= "todo-input"/>
+            <button onSubmit={submitToDoHandler} className= "todo-button" type= "submit">
+                <i className= "fas fa-plus-square"></i>
+            </button>
+            <div className= "select">
+                <select name= "todos" className= "filter-todo">
+                    <option value= "all">All</option>
+                    <option value= "completed">Completed</option>
+                    <option value= "uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+    );
+};
+
+export default Form;
