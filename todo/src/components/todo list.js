8daaff2989a35275a7d93adf9e275b60143bf070
noellemazzola <97685560+noellemazzola@users.noreply.github.com>
2022-04-23 21:58:02 -0400

    Create todo list.js

---
+++ todo/src/components/todo list.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function toDo() {
+    return(
+        <div className="todo-container">
+            <ul className="todo-list"></ul>
+        </div>
+    );
+};
+
+
+export default toDo;
